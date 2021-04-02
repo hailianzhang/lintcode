78070280131587c05644e31a6a39147b6ba03f94
hailianzhang <50775448+hailianzhang@users.noreply.github.com>
2021-04-02 16:55:41 -0500

    Update LongestDoubleAlternateSubstring.java

--- LongestDoubleAlternateSubstring.java
+++ LongestDoubleAlternateSubstring.java
@@ -40,10 +40,38 @@ public class LongestDoubleAlternateSubstring {
         }
         return flag;
     }
+    
+     public static int longestSemiAlternatingSubstring2(String s){
+        int result = 0;
+        if(s == null || s.isEmpty()){
+            return result;
+        }
+        int size = s.length();
+        if(size < 3){
+            return size;
+        }
+        int i = 0;
+        char[] charArray = s.toCharArray();
+        int count = 1;
+        for (int j = 1; j < size ; j++) {
+            char currentChar = charArray[j];
+            char preChar = charArray[j - 1];
+            //每次比较前后的字符，如果一样count++，只要不一样，count恢复初始值1
+            count = (currentChar == preChar)? (count + 1) : 1;
+            if(count == 3){
+                //如果遇到三连，将初始位置标注，把count初始值设置成2
+                i = j - 1;
+                count = 2;
+            }
+            //result用来存每次的最长长度，i为初始值的index，j为当前的index
+            result = Math.max(result, j-i+1);
+        }
+        return result;
+    }
 
     public static void main(String[] args) {
         String s = "aabbabb";
-        int i = longestSemiAlternatingSubstring(s);
+        int i = longestSemiAlternatingSubstring2(s);
         System.out.println(i);
 
 
